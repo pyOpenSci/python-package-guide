eb345879a13acd873281e0aab96c0d0b25ce3001
Leah Wasser <leah@pyopensci.org>
2022-11-23 13:11:18 -0700

    update package theme

--- conf.py
+++ conf.py
@@ -77,7 +77,7 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+html_theme = 'furo'
 html_static_path = ["_static"]
 html_css_files = ["pyos.css"]
 html_title = "pyOpenSci Package Guide"
