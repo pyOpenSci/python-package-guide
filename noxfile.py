0113c472f43b0ffcaff06997f7311ea180299a85
Leah Wasser <leah@pyopensci.org>
2022-12-28 16:20:05 -0700

    Fix: numerous cleanup edits to contributing and readme files

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,6 @@
+import os 
+import pathlib
+import shutil
 import nox
 
 nox.options.reuse_existing_virtualenvs = True
@@ -26,3 +29,21 @@ def docs_live(session):
         cmd.extend(["--ignore", f"*/{folder}/*"])
     cmd.extend(build_command + session.posargs)
     session.run(*cmd)
+
+docs_dir = os.path.join("_build", "html")
+
+@nox.session(name="docs-clean")
+def clean_dir(dir_path=docs_dir):
+    """
+    Clean out the docs directory used in the 
+    live build.
+    """
+    dir_path = pathlib.Path(dir_path)
+    dir_contents = dir_path.glob('*')
+    
+    for content in dir_contents:
+        print(content)
+        if content.is_dir():
+            shutil.rmtree(content)
+        else:
+            os.remove(content)
