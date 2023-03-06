ef2f1dc637887ccca8bd9dd1d4420ebf3efd9a8b
Leah Wasser <leah@pyopensci.org>
2023-03-06 10:57:23 -0700

    Add matomo to guide

--- conf.py
+++ conf.py
@@ -128,10 +128,6 @@ html_baseurl = 'https://www.pyopensci.org/package-review-guide/'
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ["_static"]
 html_css_files = ["pyos.css"]
+html_js_files = ["matomo.js"]
 html_title = "pyOpenSci Package Guide"
 html_logo = "images/logo/logo.png"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
