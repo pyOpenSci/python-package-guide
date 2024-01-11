84df946c631988ba2784fd6034257f9bde2ddde4
Leah Wasser <leah@pyopensci.org>
2024-01-11 09:09:55 -0700

    Fix: card year should be https

--- conf.py
+++ conf.py
@@ -146,7 +146,7 @@ html_js_files = ["matomo.js"]
 
 
 # Social cards
-ogp_site_url = "http://www.pyopensci.org/python-package-guide/"
+ogp_site_url = "https://www.pyopensci.org/python-package-guide/"
 ogp_social_cards = {
     "line_color": "#6D597A",
     "image": "_static/pyopensci-logo-package-guide.png",
