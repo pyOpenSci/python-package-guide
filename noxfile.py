45c143b4363fdede50c236e1f3ba0564c55b5881
Felipe Moreno <felipe@flpm.dev>
2024-06-06 09:13:06 -0400

    Cleaned up the current noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -5,79 +5,62 @@ import nox
 
 nox.options.reuse_existing_virtualenvs = True
 
-OUTPUT_DIR = "_build"
-docs_dir = os.path.join("_build", "html")
-build_command = ["-b", "html", ".", docs_dir]
+## Sphinx related options
+
+# Sphinx output and source directories
+OUTPUT_DIR = pathlib.Path("_build", "html")
+SOURCE_DIR = pathlib.Path(".")
+
+# Sphinx build commands
+SPHINX_BUILD = "sphinx-build"
+SPHINX_AUTO_BUILD = "sphinx-autobuild"
+
+# Sphinx parameters used to build the guide
+BUILD_PARAMETERS = ["-b", "html"]
+
+# Sphinx parameters used to test the build of the guide
+TEST_PARAMETERS = ['-W', '--keep-going', '-E', '-a']
+
+# Sphinx-autobuild ignore and include parameters
+AUTOBUILD_IGNORE = [
+    "_build",
+    "build_assets",
+    "tmp",
+]
+AUTOBUILD_INCLUDE = [
+    pathlib.Path("_static", "pyos.css")
+]
 
 @nox.session
 def docs(session):
+    """Build the packaging guide."""
     session.install("-e", ".")
-    cmd = ["sphinx-build"]
-    cmd.extend(build_command + session.posargs)
+    cmd = [SPHINX_BUILD, *BUILD_PARAMETERS, SOURCE_DIR, OUTPUT_DIR, *session.posargs]
     session.run(*cmd)
 
 @nox.session(name="docs-test")
 def docs_test(session):
-    """
-    Same as `docs`, but rebuild everything and fail on warnings for testing
-    """
+    """Build the packaging guide with additional parameters."""
     session.install("-e", ".")
-    cmd = ["sphinx-build"]
-    cmd.extend(['-W', '--keep-going', '-E', '-a'])
-    cmd.extend(build_command + session.posargs)
+    cmd = [SPHINX_BUILD, *BUILD_PARAMETERS, *TEST_PARAMETERS, SOURCE_DIR, OUTPUT_DIR, *session.posargs]
     session.run(*cmd)
-
 
 @nox.session(name="docs-live")
 def docs_live(session):
+    """Build and launch a local copy of the guide."""
     session.install("-e", ".")
-
-    AUTOBUILD_IGNORE = [
-        "_build",
-        "build_assets",
-        "tmp",
-    ]
-
-    # Explicitly include custom CSS in each build since
-    # sphinx doesn't think _static files should change since,
-    # well, they're static.
-    # Include these as the final `filenames` argument
-
-    AUTOBUILD_INCLUDE = [
-        os.path.join("_static", "pyos.css")
-    ]
-
-    # ----------------
-    # Assemble command
-    cmd = ["sphinx-autobuild"]
+    cmd = [SPHINX_AUTO_BUILD, *BUILD_PARAMETERS, SOURCE_DIR, OUTPUT_DIR, *session.posargs]
     for folder in AUTOBUILD_IGNORE:
         cmd.extend(["--ignore", f"*/{folder}/*"])
-
-    #cmd.extend(build_command)
-    cmd.extend(build_command + session.posargs)
-
-    # Use positional arguments if we have them
-    # if len(session.posargs) > 0:
-    #     cmd.extend(session.posargs)
-    # # Otherwise use default output and include directory
-    # else:
-    #     cmd.extend(AUTOBUILD_INCLUDE)
-
     session.run(*cmd)
 
-
-
 @nox.session(name="docs-clean")
-def clean_dir(dir_path=docs_dir):
-    """
-    Clean out the docs directory used in the
-    live build.
-    """
-    dir_path = pathlib.Path(dir_path)
-    dir_contents = dir_path.glob('*')
-
+def clean_dir(session):
+    """Clean out the docs directory used in the live build."""
+    session.warn(f"Cleaning out {OUTPUT_DIR}")
+    dir_contents = OUTPUT_DIR.glob('*')
     for content in dir_contents:
-        print(content)
+        session.log(f'removing {content}')
         if content.is_dir():
             shutil.rmtree(content)
         else:
