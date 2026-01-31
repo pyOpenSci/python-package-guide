a53f8d1738f0d79fedad740228f3364339109dba
tkoyama010 <tkoyama010@users.noreply.github.com>
2026-01-31 23:01:18 +0000

    deploy: aadb87d06df7d0f30ce32e77d89a7347616326fa

--- ja/_static/language_data.js
+++ ja/_static/language_data.js
@@ -1,19 +1,20 @@
 /*
  * This script contains the language-specific data used by searchtools.js,
- * namely the list of stopwords, stemmer, scorer and splitter.
+ * namely the set of stopwords, stemmer, scorer and splitter.
  */
 
-var stopwords = [];
+const stopwords = new Set([]);
+window.stopwords = stopwords;  // Export to global scope
 
 
-/* Non-minified version is copied as a separate JS file, if available */
+/* Non-minified versions are copied as separate JavaScript files, if available */
 
 /**
  * Dummy stemmer for languages without stemming rules.
  */
-var Stemmer = function() {
-  this.stemWord = function(w) {
+var Stemmer = function () {
+  this.stemWord = function (w) {
     return w;
-  }
-}
+  };
+};
 
